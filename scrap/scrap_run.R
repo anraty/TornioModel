79c914178bb7d1f7d6ba35c6f4ec6340a1b919c5
anraty <antti.raty97@gmail.com>
2024-02-12 22:54:22 +0200

    k

--- scrap/scrap_run.R
+++ scrap/scrap_run.R
@@ -1,10 +1,20 @@
-source("scrap/modified_model.R")
-data <- datalist
+library(tidyverse)
+library(runjags)
+library(rjags)
+#source("scrap/modified_model.R")
+source("scrap/modified_model_processE.R")
+initsall <- readRDS("scrap/inits.rds")
+
+#data <- datalist
 data$stock <- 1
 run0 <- run.jags(WGBAST_model, monitor= parnames,
-                 data=data,#inits = initsall,
+                 data=data,inits = initsall,
                  n.chains = 2, method = 'parallel', thin=1,
                  burnin =10000, modules = "mix",
                  sample =10, adapt = 10000,
                  keep.jags.files=F,#paste0(runName, assessment_year),
                  progress.bar=TRUE, jags.refresh=1)
+
+sink(file = "scrap/failedJAGS.txt")
+failed.jags()
+sink()
